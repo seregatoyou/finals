ab27d20a59f3f69dd378a4f238725958276624e4
Sergei <spanyukhin@list.ru>
2024-05-27 12:20:12 +0700

    Этап 4. Программа.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+Console.WriteLine("Вам представлена возможность ввести 3 слова. Мы начинаем.");
+string[] stringArray = new string[3];
+int sum = 0;
+for(int i = 0; i < stringArray.Length; i++)
+{
+    Console.WriteLine("Введите текст:");
+    stringArray[i] = Console.ReadLine();
+    if(stringArray[i].Length <= 3){
+        sum++;
+    }
+}
+Console.WriteLine("Ваши варианты: ");
+Console.WriteLine(String.Join("\n", stringArray));
+
+string[] stringArray2 = new string[sum];
+int x = 0;
+for(int i = 0; i < stringArray.Length; i++)
+{
+if(stringArray[i].Length <= 3){
+        stringArray2[x] = stringArray[i];
+        x++;
+    }
+}
+Console.WriteLine("Корректировка: ");
+Console.WriteLine(String.Join("\n", stringArray2));
